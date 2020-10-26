8ae7bc0b97cd63a6ba9a5ad24d1eae65c7094668
Wombat42 <andy@savvyduck.com>
2020-10-26 16:16:32 -0400

    Revert "added second div div"
    This reverts commit 3e64f935044dc5fbe5777d61379b0de1c13bb7df.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ export default function App() {
       <h1>Hello CI Demo</h1>
       <h2>Some features will get added here to show it all working!</h2>
       <div>This is where the first new feature will go</div>
-      <div>Adding second new div</div>
-      <div>this is where my second feature will go</div>
     </div>
   );
 }

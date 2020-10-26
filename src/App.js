67d2f711a0cc3111ec25e176623ee1243757be22
Wombat42 <andy@savvyduck.com>
2020-10-26 16:20:47 -0400

    New heading tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles.css';
+import {Heading} from './heading'
 
 export default function App() {
   return (
     <div className='App'>
-      <h1>Hello CI Demo</h1>
+      <Heading>Hello CI Demo!</Heading>
       <h2>Some features will get added here to show it all working!</h2>
       <div>This is where the first new feature will go</div>
     </div>
---
+++ src/heading.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export function Heading(props) {
+  return <p>{props.children}</p>;
+}

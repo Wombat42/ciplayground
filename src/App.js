f769406b40ad850cb5f0aadf9b374ccac3b41f44
Wombat42 <andy@savvyduck.com>
2020-10-26 14:58:27 -0400

    Some new code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className='App'>
+      <h1>Hello CI Demo</h1>
+      <h2>Some features will get added here to show it all working!</h2>
     </div>
   );
 }
